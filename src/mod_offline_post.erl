d68dcc203f48531043782faad705704d62ec50e8
sureshprasanna70 <sureshprasanna70@gmail.com>
2014-10-15 18:27:35 +0530

    Working offline hook

--- src/mod_offline_post.erl
+++ src/mod_offline_post.erl
@@ -14,66 +14,93 @@
 %%% License, or (at your option) any later version.
 %%%
 %%% This program is distributed in the hope that it will be useful,
-%%% but WITHOUT ANY WARRANTY; without even the implied warranty of
-%%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-%%% General Public License for more details.
-%%%
-%%% You should have received a copy of the GNU General Public License
-%%% along with this program; if not, write to the Free Software
-%%% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-%%% 02111-1307 USA
-%%%
-%%%----------------------------------------------------------------------
+	%%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+	%%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	%%% General Public License for more details.
+	%%%
+	%%% You should have received a copy of the GNU General Public License
+	%%% along with this program; if not, write to the Free Software
+	%%% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+	%%% 02111-1307 USA
+	%%%
+	%%%----------------------------------------------------------------------
 
--module(mod_offline_post).
--author('rgeorge@midnightweb.net').
+	-module(mod_offline_post).
+	-author('rgeorge@midnightweb.net').
 
--behaviour(gen_mod).
+	-behaviour(gen_mod).
 
--export([start/2,
-	 init/2,
-	 stop/1,
-	 send_notice/3]).
+	-export([start/2,
+			init/2,
+			stop/1,
+			send_notice/3]).
 
--define(PROCNAME, ?MODULE).
+	-define(PROCNAME, ?MODULE).
 
--include("ejabberd.hrl").
--include("jlib.hrl").
+	-include("ejabberd.hrl").
+	-include("jlib.hrl").
 
-start(Host, Opts) ->
-    ?INFO_MSG("Starting mod_offline_post", [] ),
-    register(?PROCNAME,spawn(?MODULE, init, [Host, Opts])),  
-    ok.
+	start(Host, Opts) ->
+	  register(?PROCNAME,spawn(?MODULE, init, [Host, Opts])),  
+	  inets:start(),
+	ok.
 
-init(Host, _Opts) ->
-    inets:start(),
-    ssl:start(),
-    ejabberd_hooks:add(offline_message_hook, Host, ?MODULE, send_notice, 10),
-    ok.
+	init(Host, _Opts) ->
+	   inets:start(),
+	   ssl:start(),
+	   ejabberd_hooks:add(offline_message_hook, Host, ?MODULE, send_notice, 30),
+	ok.
 
-stop(Host) ->
-    ?INFO_MSG("Stopping mod_offline_post", [] ),
-    ejabberd_hooks:delete(offline_message_hook, Host,
-			  ?MODULE, send_notice, 10),
-    ok.
+	stop(Host) ->
+	  ejabberd_hooks:delete(offline_message_hook, Host,?MODULE, send_notice, 30),
+	ok.
 
-send_notice(_From, To, Packet) ->
-    Type = xml:get_tag_attr_s("type", Packet),
-    Body = xml:get_path_s(Packet, [{elem, "body"}, cdata]),
-    Token = gen_mod:get_module_opt(To#jid.lserver, ?MODULE, auth_token, [] ),
-    PostUrl = gen_mod:get_module_opt(To#jid.lserver, ?MODULE, post_url, [] ),
-    if
-	(Type == "chat") and (Body /= "") ->
-	  Sep = "&",
-	  Post = [
-	    "to=", To#jid.luser, Sep,
-	    "from=", _From#jid.luser, Sep,
-	    "body=", Body, Sep,
-	    "access_token=", Token ],
-	  ?INFO_MSG("Sending post request ~p~n",[Post] ),
-	  httpc:request(post, {PostUrl, [], "application/x-www-form-urlencoded", list_to_binary(Post)},[],[]),
-	  ok;
-	true ->
-	  ok
-    end.
-
+	send_notice(_From, To, Packet) ->
+		 Body = xml:get_path_s(Packet, [{elem, list_to_binary("body")}, cdata]),
+		 Type = xml:get_tag_attr_s(list_to_binary("type"), Packet),
+		PostUrl = "http://54.183.143.220/testpush.php",
+		 if (Type == <<"chat">>) and (Body /= <<"">>) ->
+			Sep = "&",
+			Post = [
+				"to=", To#jid.luser, Sep,
+				"from=", _From#jid.luser, Sep,
+				"body=", url_encode(binary_to_list(Body)), Sep],
+				httpc:request(post, {PostUrl, [], "application/x-www-form-urlencoded", list_to_binary(Post)},[],[]),
+				ok;
+			true ->
+			ok
+		end.
+url_encode([H|T]) when is_list(H) ->
+[url_encode(H) | url_encode(T)];
+url_encode([H|T]) ->
+if
+H >= $a, $z >= H ->
+[H|url_encode(T)];
+H >= $A, $Z >= H ->
+[H|url_encode(T)];
+H >= $0, $9 >= H ->
+[H|url_encode(T)];
+H == $_; H == $.; H == $-; H == $/; H == $: -> % FIXME: more..
+[H|url_encode(T)];
+true ->
+case integer_to_hex(H) of
+[X, Y] ->
+[$%, X, Y | url_encode(T)];
+[X] ->
+[$%, $0, X | url_encode(T)]
+end
+end;
+url_encode([]) ->
+[].
+integer_to_hex(I) ->
+case catch erlang:integer_to_list(I, 16) of
+{'EXIT', _} -> old_integer_to_hex(I);
+Int -> Int
+end.
+old_integer_to_hex(I) when I < 10 ->
+integer_to_list(I);
+old_integer_to_hex(I) when I < 16 ->
+[I-10+$A];
+old_integer_to_hex(I) when I >= 16 ->
+N = trunc(I/16),
+old_integer_to_hex(N) ++ old_integer_to_hex(I rem 16).	
